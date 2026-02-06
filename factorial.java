e2d23bb6ed07846e5585dc623667cac630071ab9
mohanrao49 <boddumohanrao.23.it@anits.edu.in>
2026-02-06 13:42:18 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,22 +1,14 @@
-import java.util.Scanner;
-
 public class factorial {
 
     public static void main(String[] args) {
 
-        int number, factorial = 1;
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        number = sc.nextInt();
+        int number = 5;   // hardcoded value
+        int factorial = 1;
 
         for(int i = 1; i <= number; i++) {
             factorial = factorial * i;
         }
 
         System.out.println("Factorial of " + number + " is: " + factorial);
-
-        sc.close();
     }
 }
