600e552ed14d146d9ab61e4e25d5ca63221491aa
mohanrao49 <boddumohanrao.23.it@anits.edu.in>
2026-02-20 13:19:05 +0530

    Add factorial calculation in factorial.java

---
+++ factorial.java
@@ -0,0 +1,14 @@
+public class factorial {
+
+    public static void main(String[] args) {
+
+        int number = 5;   // hardcoded value
+        int factorial = 1;
+
+        for(int i = 1; i <= number; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+}
